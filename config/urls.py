5c22779464aee585a305c8ae58e792a0189a048a
andres <inge.andrespinzon@gmail.com>
2021-01-21 19:17:25 -0500

    Feat | define urls

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from core.urls import dataset_urlpatterns, row_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include([
+        path('datasets/', include(dataset_urlpatterns)),
+        path('rows/', include(row_urlpatterns)),
+    ]))
 ]
---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+dataset_urlpatterns = [
+    path('', )
+]
+
+row_urlpatterns = [
+    path('', )
+]
