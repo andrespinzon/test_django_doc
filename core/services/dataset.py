427e28a4ce4a004b4f5b648051ccfcd13d04760e
andres <inge.andrespinzon@gmail.com>
2021-01-21 19:33:18 -0500

    Feat | add method to service

--- core/services/dataset.py
+++ core/services/dataset.py
@@ -1,8 +1,21 @@
 from typing import Dict
 
+from common.validations import validate_allowed_fields, validate_allowed_type_files, \
+    validate_allowed_fields_filters, validate_required_fields
+
 
 class DatasetService:
 
-    @staticmethod
-    def upload_dataset(file: Dict):
+    _data: Dict
+
+    def upload_dataset(self, file: Dict):
+        self._data = file
+
+        fields = {'file'}
+        validate_required_fields(data=self._data, required_fields=fields)
+        validate_allowed_fields(data=self._data, allowed_fields=fields)
+        validate_allowed_type_files(data=self._data, field='file', type_files={'csv'})
+
+
+
         pass
